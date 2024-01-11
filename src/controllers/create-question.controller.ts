8c6ad222739083b7b675ed85341f212b4299a0d2
Danrlei <danrleidalfre@gmail.com>
2024-01-11 19:53:57 -0300

    add middleware to auth on create question

---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Post, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { CurrentUser } from '../auth/current-user-decorator'
+import { UserPayload } from '../auth/jwt.strategy'
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor() {}
+
+  @Post()
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user.sub)
+    return 'ok'
+  }
+}
