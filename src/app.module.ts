0875add3238197b26f7becd154296dfbad229974
Danrlei <danrleidalfre@gmail.com>
2024-01-11 21:56:07 -0300

    implement tests authenticate, fetch questions and create question

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { envSchema } from './env'
 import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
-import { FetchQuestionsController } from './controllers/fetch-question.controller'
+import { FetchQuestionsController } from './controllers/fetch-questions.controller'
 
 @Module({
   imports: [
