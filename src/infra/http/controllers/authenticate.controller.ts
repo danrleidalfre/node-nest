f301413f17ffe75d29a15cd77b8f44f24a1d9e9c
Danrlei <danrleidalfre@gmail.com>
2024-01-13 21:28:27 -0300

    create public routes check

--- src/infra/http/controllers/authenticate.controller.ts
+++ src/infra/http/controllers/authenticate.controller.ts
@@ -10,6 +10,7 @@ import { z } from 'zod'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation'
 import { AuthenticateStudentUseCase } from '@/domain/forum/enterprise/entities/authenticate-student'
 import { WrongCredentialsError } from '@/domain/forum/enterprise/entities/errors/wrong-credentials-error'
+import { Public } from '@/infra/auth/public'
 
 const authenticateBodySchema = z.object({
   email: z.string().email(),
@@ -19,6 +20,7 @@ const authenticateBodySchema = z.object({
 type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>
 
 @Controller('/sessions')
+@Public()
 export class AuthenticateController {
   constructor(private authenticateStudent: AuthenticateStudentUseCase) {}
 
